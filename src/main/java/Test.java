7050147c8466f03ed159e1f6ab1c78f1c43319ee
Pradeep LN <pradeep.ln78@gmail.com>
2022-05-23 12:55:02 +0530

    simulating conflict

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		//This is a conflicting change
-		System.out.println("Hello , Git!!");
+		System.out.println("Hello , Git!! 1");
 		//comment added in github site editor
 	}
 
