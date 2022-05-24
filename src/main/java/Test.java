75f6038c6265ac7c9eaf6cfebf3f4383de932262
pradeepln <41988925+pradeepln@users.noreply.github.com>
2022-05-24 08:21:05 +0530

    with a remote comment

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@
 public class Test {
 	
 	public static void main(String[] args) {
+		//this is a comment in remote repo
 		System.out.println("Hello wait, Git!!");
 		//comment added in github site editor
 	}
